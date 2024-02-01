62fe6a0accd07c33d43eaa358e92902e00d6e86a
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-01 12:16:41 +0530

    Blog app using appwrite initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold text-gray-500 bg-black text-center p-4">
+        A blog app with Appwrite
+      </h1>
+    </>
+  );
+}
+
+export default App;
