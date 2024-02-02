40c9c1526672d4017e642047dcf3242511831df0
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-02 12:39:57 +0530

    User authentication services are completed. Prettier format is defined

---
+++ src/appwrite/auth.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-useless-catch */
+/* eslint-disable no-unused-vars */
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+class AuthService {
+   client = new Client();
+   account;
+
+   AuthService() {
+      this.client
+         .setEndpoint(conf.appwriteUrl)
+         .setProject(conf.appwriteProjectId);
+
+      this.account = new Account(this.client);
+   }
+
+   async createAccount({ email, password, name }) {
+      try {
+         const userAccount = await this.account.create(
+            ID.unique(),
+            email,
+            password,
+            name
+         );
+
+         if (userAccount) {
+            this.login(email, password);
+         } else {
+            return userAccount;
+         }
+      } catch (error) {
+         throw error;
+      }
+   }
+
+   async login({ email, password }) {
+      try {
+         return await this.account.createEmailSession(email, password);
+      } catch (error) {
+         throw error;
+      }
+   }
+
+   async getCurrentUser() {
+      try {
+         return await this.account.get();
+      } catch (error) {
+         console.log("Appwrite server error :: getCurrentUser", error);
+      }
+
+      return null;
+   }
+
+   async logOut() {
+      try {
+         return await this.account.deleteSessions();
+      } catch (error) {
+         console.log("Error in LogOut", error);
+      }
+   }
+}
+
+const authService = new AuthService();
+
+export default authService;
