0844e01ccab38d9730445b07e522c6f21d7483c5
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-14 14:28:05 +0530

    Routers are added using AuthLayout and enum authentication values true and false.

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,80 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { AuthLayout } from "./components/index.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+   AddPost,
+   AllPost,
+   Home,
+   EditPost,
+   Login,
+   SignUp,
+   Post,
+} from "./Pages/index.js";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <App />,
+      children: [
+         {
+            path: "/",
+            element: <Home />,
+         },
+         {
+            path: "/login",
+            element: (
+               <AuthLayout>
+                  <Login authentication={false} />
+               </AuthLayout>
+            ),
+         },
+         {
+            path: "/signup",
+            element: (
+               <AuthLayout authentication={false}>
+                  <SignUp />
+               </AuthLayout>
+            ),
+         },
+         {
+            path: "/all-posts",
+            element: (
+               <AuthLayout authentication>
+                  {" "}
+                  <AllPost />
+               </AuthLayout>
+            ),
+         },
+         {
+            path: "/add-post",
+            element: (
+               <AuthLayout authentication>
+                  {" "}
+                  <AddPost />
+               </AuthLayout>
+            ),
+         },
+         {
+            path: "/edit-post/:slug",
+            element: (
+               <AuthLayout authentication>
+                  {" "}
+                  <EditPost />
+               </AuthLayout>
+            ),
+         },
+         {
+            path: "/post/:slug",
+            element: <Post />,
+         },
+      ],
+   },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
    </Provider>
 );
